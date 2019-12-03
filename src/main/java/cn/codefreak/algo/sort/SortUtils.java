29ca4ca7e9efc84fb51dd83d9909a6b58aeaeaa6
opendraognhuang <opendragonhuang@outlook.com>
2019-12-03 20:17:32 +0800

    添加归并排序和快速排序

--- src/main/java/cn/codefreak/algo/sort/SortUtils.java
+++ src/main/java/cn/codefreak/algo/sort/SortUtils.java
@@ -1,6 +1,7 @@
 package cn.codefreak.algo.sort;
 
 import java.io.Serializable;
+import java.util.Arrays;
 
 /**
  * 基本排序算法实现
@@ -71,4 +72,131 @@ public class SortUtils implements Serializable {
             }
         }
     }
+
+    /**
+     * 归并排序
+     * @param elements
+     * @param <E>
+     */
+    public static <E extends Comparable<E>> void merge(E[] elements) {
+        mergeSort(elements, 0, elements.length-1);
+    }
+
+    /**
+     * 归并排序重载方法
+     * @param elements
+     * @param start
+     * @param end
+     * @param <E>
+     */
+    private static <E extends Comparable<E>> void mergeSort(E[] elements, int start, int end) {
+        if (start >= end) return;
+
+        int mid = start + (end-start)/2;
+        mergeSort(elements, start, mid);
+        mergeSort(elements, mid+1, end);
+        _merge(elements, start, mid, end);
+    }
+
+    /**
+     * 合并数组两个有序的部分, 不带哨兵
+     * @param elements
+     * @param start
+     * @param mid
+     * @param end
+     * @param <E>
+     */
+    private static <E extends Comparable<E>> void _merge(E[] elements, int start, int mid, int end) {
+        E[] temps = (E[])new Comparable[end - start + 1];
+        int i = start;
+        int j = mid+1;
+        int k = 0;
+        while (i <= mid && j <= end){
+            if(elements[i].compareTo(elements[j]) < 0){
+                temps[k++] = elements[i++];
+            }else{
+                temps[k++] = elements[j++];
+            }
+        }
+
+        while (i <= mid)
+            temps[k++] = elements[i++];
+        while (j <= end)
+            temps[k++] = elements[j++];
+
+        i = start;k = 0;
+        while (i <= end)
+            elements[i++] = temps[k++];
+    }
+
+//    /**
+//     * 合并数组中两个有序的部分，使用哨兵方式, 目前不完整
+//     * @param elements
+//     * @param start
+//     * @param mid
+//     * @param end
+//     * @param <E>
+//     */
+//    private static <E extends Comparable<E>> void _merge(E[] elements, int start, int mid, int end) {
+//        // 左边有序数组的大小
+//        int leftSize = mid - start + 1;
+//        // 右边有序数组的大小
+//        int rightSize = end - mid;
+//
+//        E[] leftTemps = (E[])new Comparable[leftSize+1];
+//        E[] rightTemps = (E[])new Comparable[rightSize+1];
+//        System.arraycopy(elements, start, leftTemps, mid, leftSize);
+//        System.arraycopy(elements, mid+1, rightTemps, end, rightSize);
+//
+//    }
+
+    /**
+     * 快速排序
+     * @param elements
+     * @param <E>
+     */
+    public static <E extends Comparable<E>> void quick(E[] elements) {
+        quickSort(elements, 0, elements.length-1);
+    }
+
+    /**
+     * 快速排序实现
+     * @param elements
+     * @param start
+     * @param end
+     * @param <E>
+     */
+    private static <E extends Comparable<E>> void quickSort(E[] elements, int start, int end){
+        if(start >= end) return;
+
+        int pivotIndex = partition(elements, start, end);
+        quickSort(elements, start, pivotIndex-1);
+        quickSort(elements, pivotIndex+1, end);
+    }
+
+    /**
+     * 获取分区索引
+     * @param elements
+     * @param start
+     * @param end
+     * @param <E>
+     * @return
+     */
+    private static <E extends Comparable<E>> int partition(E[] elements, int start, int end){
+        E pivot = elements[end];
+        int i = start;
+        int j = start;
+        while (j <= end){
+            if(elements[j].compareTo(pivot) < 0){
+                swap(elements, i, j);
+                i++;
+            }
+            j++;
+        }
+        swap(elements, i, end);
+        return i;
+    }
+
+
+
 }
--- src/test/java/cn/codefreak/algo/sort/SortUtilsTest.java
+++ src/test/java/cn/codefreak/algo/sort/SortUtilsTest.java
@@ -1,5 +1,7 @@
 package cn.codefreak.algo.sort;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -8,7 +10,11 @@ import java.util.Random;
 import static org.junit.Assert.*;
 
 public class SortUtilsTest {
-    public Integer[] createRandomNumbers(int count, int bound){
+    private static Integer[] src;
+    private static final int count = 5000;
+    private static final int bound = 5000;
+    
+    public static Integer[] createRandomNumbers(int count, int bound){
         Random r = new Random();
         Integer[] randomNumbers = new Integer[count];
         for (int i = 0; i < count; i++) {
@@ -17,54 +23,87 @@ public class SortUtilsTest {
         return randomNumbers;
     }
 
+    @BeforeClass
+    public static void initNumbers(){
+        src = createRandomNumbers(count, bound);
+    }
+
     @Test
     public void bubble() {
         System.out.println("bubble sort test");
-        Integer[] src = createRandomNumbers(10, 100);
-        System.out.println("src:"+ Arrays.toString(src));
-
+        
         Integer[] sort1 = Arrays.copyOf(src, src.length);
         Arrays.sort(sort1);
-        System.out.println("sort1:"+Arrays.toString(sort1));
 
         Integer[] sort2 = Arrays.copyOf(src, src.length);
+        long start = System.currentTimeMillis();
         SortUtils.bubble(sort2);
-        System.out.println("sort2:"+Arrays.toString(sort2));
+        long end = System.currentTimeMillis();
 
         assertArrayEquals(sort1, sort2);
+        System.out.println("bubble sort test time:"+ (end - start) + "ms");
     }
 
     @Test
     public void insertion() {
-        System.out.println("insertion test");
-        Integer[] src = createRandomNumbers(10, 100);
-        System.out.println("src:"+ Arrays.toString(src));
-
+        System.out.println("insertion sort test");
         Integer[] sort1 = Arrays.copyOf(src, src.length);
         Arrays.sort(sort1);
-        System.out.println("sort1:"+Arrays.toString(sort1));
 
         Integer[] sort2 = Arrays.copyOf(src, src.length);
-        SortUtils.bubble(sort2);
-        System.out.println("sort2:"+Arrays.toString(sort2));
+        long start = System.currentTimeMillis();
+        SortUtils.insertion(sort2);
+        long end = System.currentTimeMillis();
 
         assertArrayEquals(sort1, sort2);
+        System.out.println("insertion sort test time:"+ (end - start) + "ms");
     }
 
     @Test
     public void selection() {
-        System.out.println("selection test");
-        Integer[] src = createRandomNumbers(10, 100);
-        System.out.println("src:"+ Arrays.toString(src));
-
+        System.out.println("selection sort test");
         Integer[] sort1 = Arrays.copyOf(src, src.length);
         Arrays.sort(sort1);
-        System.out.println("sort1:"+Arrays.toString(sort1));
 
         Integer[] sort2 = Arrays.copyOf(src, src.length);
+
+        long start = System.currentTimeMillis();
         SortUtils.selection(sort2);
-        System.out.println("sort2:"+Arrays.toString(sort2));
+        long end = System.currentTimeMillis();
 
         assertArrayEquals(sort1, sort2);
+        System.out.println("selection sort test time:"+ (end - start) + "ms");
+    }
+
+    @Test
+    public void merge() {
+        System.out.println("merge sort test");
+        Integer[] sort1 = Arrays.copyOf(src, src.length);
+        Arrays.sort(sort1);
+
+        Integer[] sort2 = Arrays.copyOf(src, src.length);
+
+        long start = System.currentTimeMillis();
+        SortUtils.merge(sort2);
+        long end = System.currentTimeMillis();
+
+        assertArrayEquals(sort1, sort2);
+        System.out.println("merge sort test time:"+ (end - start) + "ms");
+    }
+
+    @Test
+    public void quick() {
+        System.out.println("quick sort test");
+        Integer[] sort1 = Arrays.copyOf(src, src.length);
+        Arrays.sort(sort1);
+
+        Integer[] sort2 = Arrays.copyOf(src, src.length);
+
+        long start = System.currentTimeMillis();
+        SortUtils.quick(sort2);
+        long end = System.currentTimeMillis();
+
+        assertArrayEquals(sort1, sort2);
+        System.out.println("quick sort test time:"+ (end - start) + "ms");
     }
 }
